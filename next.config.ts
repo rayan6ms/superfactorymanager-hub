cc0d6e26172a2d0b6acaef35aacf170fbc4c734c
rayan6ms <rayan6ms@gmail.com>
2025-12-02 18:11:39 -0300

    Fix vercel prisma bug2

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/.prisma/client/**/*'],
+    '/api/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
