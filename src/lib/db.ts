ddb7b705c96416d92e4c16737a7e3d0cd649ba38
rayan6ms <rayan6ms@gmail.com>
2026-02-14 15:12:39 -0300

    fix prisma accelerate error & update next + more packages

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,2 +1,13 @@
+import "server-only";
 import { PrismaClient } from "@prisma/client";
-export const db = new PrismaClient();
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+export const db = prisma.$extends(withAccelerate()) as unknown as PrismaClient;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
